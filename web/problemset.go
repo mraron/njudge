ad22cf59d21dbeb95567511ea40899079817c1c1
Áron Noszály <noszalyaron4@gmail.com>
2021-01-11 17:56:54 +0100

    Prettier task archive

--- web/problemset.go
+++ web/problemset.go
@@ -145,6 +145,7 @@ func (s *Server) getProblemsetStatus(c echo.Context) error {
 }
 
 type taskArchiveTreeNode struct {
+	Id       int
 	Type     string
 	Name     string
 	Link     string
@@ -161,6 +162,7 @@ func (s *Server) getTaskArchive(c echo.Context) error {
 	roots := make([]*taskArchiveTreeNode, 0)
 
 	var dfs func(category *models.ProblemCategory, node *taskArchiveTreeNode) error
+	id := 1000
 	dfs = func(root *models.ProblemCategory, tree *taskArchiveTreeNode) error {
 		problems, err := models.ProblemRels(Where("category_id = ?", root.ID), OrderBy("problem")).All(s.db)
 		if err != nil {
@@ -168,7 +170,8 @@ func (s *Server) getTaskArchive(c echo.Context) error {
 		}
 
 		for _, problem := range problems {
-			tree.Children = append(tree.Children, &taskArchiveTreeNode{"problem", translateContent("hungarian", s.getProblem(problem.Problem).Titles()).String(), fmt.Sprintf("/problemset/%s/%s/", problem.Problemset, problem.Problem), make([]*taskArchiveTreeNode, 0)})
+			tree.Children = append(tree.Children, &taskArchiveTreeNode{id, "problem", translateContent("hungarian", s.getProblem(problem.Problem).Titles()).String(), fmt.Sprintf("/problemset/%s/%s/", problem.Problemset, problem.Problem), make([]*taskArchiveTreeNode, 0)})
+			id++
 		}
 
 		//@TODO make a way to control sorting order from db (add migrations etc.)
@@ -178,7 +181,7 @@ func (s *Server) getTaskArchive(c echo.Context) error {
 		}
 
 		for _, cat := range subcats {
-			akt := &taskArchiveTreeNode{"category", cat.Name, "", make([]*taskArchiveTreeNode, 0)}
+			akt := &taskArchiveTreeNode{cat.ID, "category", cat.Name, "", make([]*taskArchiveTreeNode, 0)}
 			tree.Children = append(tree.Children, akt)
 			if err := dfs(cat, akt); err != nil {
 				return err
@@ -189,7 +192,7 @@ func (s *Server) getTaskArchive(c echo.Context) error {
 	}
 
 	for _, start := range lst {
-		roots = append(roots, &taskArchiveTreeNode{"category", start.Name, "", make([]*taskArchiveTreeNode, 0)})
+		roots = append(roots, &taskArchiveTreeNode{start.ID, "category", start.Name, "", make([]*taskArchiveTreeNode, 0)})
 		if dfs(start, roots[len(roots)-1]) != nil {
 			return s.internalError(c, err, "Belső hiba.")
 		}
