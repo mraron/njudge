7e4967f312ed6483c4f5596b5c8de3990d877f3b
Áron Noszály <noszalyaron4@gmail.com>
2021-07-13 20:10:59 +0200

    Separate routing out from server.go

--- web/server.go
+++ web/server.go
@@ -74,74 +74,7 @@ func (s *Server) Run() {
 
 	e.Renderer = templates.New(s.TemplatesDir, s.ProblemStore)
 
-	e.GET("/", s.getHome)
-
-	e.Static("/static", "public")
-
-	e.GET("/submission/:id", s.getSubmission)
-	e.GET("/submission/rejudge/:id", s.getSubmissionRejudge)
-	e.GET("/task_archive", s.getTaskArchive)
-
-	ps := e.Group("/problemset", func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			c.Set("problemset", c.Param("name"))
-			return next(c)
-		}
-	})
-
-	ps.GET("/:name/", s.getProblemsetList)
-	ps.GET("/:name/:problem/", s.getProblemsetProblem)
-	ps.GET("/:name/:problem/problem", s.getProblemsetProblem)
-	ps.GET("/:name/:problem/status", s.getProblemsetProblemStatus)
-	ps.GET("/:name/:problem/ranklist", s.getProblemsetProblemRanklist)
-	ps.GET("/:name/:problem/pdf/:language/", s.getProblemsetProblemPDFLanguage)
-	ps.GET("/:name/:problem/attachment/:attachment/", s.getProblemsetProblemAttachment)
-	ps.GET("/:name/:problem/:file", s.getProblemsetProblemFile)
-	ps.POST("/:name/submit", s.postProblemsetSubmit)
-	ps.GET("/status", s.getProblemsetStatus)
-
-	u := e.Group("/user")
-
-	u.GET("/auth/callback", s.getUserAuthCallback)
-	u.GET("/auth", s.getUserAuth)
-
-	u.GET("/login", s.getUserLogin)
-	u.POST("/login", s.postUserLogin)
-	u.GET("/logout", s.getUserLogout)
-	u.GET("/register", s.getUserRegister)
-	u.POST("/register", s.postUserRegister)
-	u.GET("/activate", s.getUserActivate)
-	u.GET("/activate/:name/:key", s.getActivateUser)
-
-	u.GET("/profile/:name/", s.getUserProfile)
-
-	v1 := e.Group("/api/v1")
-
-	v1.GET("/problem_rels", s.getAPIProblemRels)
-	v1.POST("/problem_rels", s.postAPIProblemRel)
-	v1.GET("/problem_rels/:id", s.getAPIProblemRel)
-	v1.PUT("/problem_rels/:id", s.putAPIProblemRel)
-	v1.DELETE("/problem_rels/:id", s.deleteAPIProblemRel)
-
-	v1.GET("/judges", s.getAPIJudges)
-	v1.POST("/judges", s.postAPIJudge)
-	v1.GET("/judges/:id", s.getAPIJudge)
-	v1.PUT("/judges/:id", s.putAPIJudge)
-	v1.DELETE("/judges/:id", s.deleteAPIJudge)
-
-	v1.GET("/users", s.getAPIUsers)
-	v1.POST("/users", s.postAPIUser)
-	v1.GET("/users/:id", s.getAPIUser)
-	v1.PUT("/users/:id", s.putAPIUser)
-	v1.DELETE("/users/:id", s.deleteAPIUser)
-
-	v1.GET("/submissions", s.getAPISubmissions)
-	v1.POST("/submissions", s.postAPISubmission)
-	v1.GET("/submissions/:id", s.getAPISubmission)
-	v1.PUT("/submissions/:id", s.putAPISubmission)
-	v1.DELETE("/submissions/:id", s.deleteAPISubmission)
-
-	e.GET("/admin", s.getAdmin)
+	s.prepareRoutes(e)
 
 	panic(e.Start(":" + s.Port))
 }
