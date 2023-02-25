2bd2ff24f9ae2f1b5b2daba8d3debdaa79be55ce
Áron Noszály <noszalyaron4@gmail.com>
2023-02-25 18:13:20 +0100

    Add way to add custom pages with custom content to the site

--- internal/web/routes.go
+++ internal/web/routes.go
@@ -3,6 +3,7 @@ package web
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/mraron/njudge/internal/web/extmodels"
+	"github.com/mraron/njudge/internal/web/handlers"
 	"github.com/mraron/njudge/internal/web/handlers/api"
 	"github.com/mraron/njudge/internal/web/handlers/problemset"
 	"github.com/mraron/njudge/internal/web/handlers/problemset/problem"
@@ -10,13 +11,16 @@ import (
 	"github.com/mraron/njudge/internal/web/handlers/taskarchive"
 	"github.com/mraron/njudge/internal/web/handlers/user"
 	"github.com/mraron/njudge/internal/web/handlers/user/profile"
+	"github.com/mraron/njudge/internal/web/helpers/templates/partials"
 	"github.com/mraron/njudge/internal/web/models"
+	"time"
 )
 
 func (s *Server) prepareRoutes(e *echo.Echo) {
 	e.Use(user.SetUserMiddleware(s.DB))
 
-	e.GET("/", s.getHome)
+	e.GET("/", handlers.GetHome())
+	e.GET("/page/:page", handlers.GetPage(partials.NewCached(s.DB.DB, 30*time.Second)))
 
 	e.Static("/static", "static")
 
@@ -95,5 +99,5 @@ func (s *Server) prepareRoutes(e *echo.Echo) {
 	v1.PUT("/submissions/:id", api.Put[models.Submission](submissionDataProvider))
 	v1.DELETE("/submissions/:id", api.Delete[models.Submission](submissionDataProvider))
 
-	e.GET("/admin", s.getAdmin)
+	e.GET("/admin", handlers.GetAdmin(s.Server))
 }
