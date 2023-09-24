081c38d2e86d505d225434897a348b430127f627
dbence2002 <thelastsugondese@gmail.com>
2023-09-24 14:51:38 +0200

    storing theme preference in local storage

--- frontend/src/contexts/theme/ThemeProvider.js
+++ frontend/src/contexts/theme/ThemeProvider.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import ThemeContext from "./ThemeContext";
 
 function ThemeProvider({ children }) {
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+    useEffect(() => {
+        changeTheme(theme)
+    }, []);
 
     const changeTheme = (newTheme) => {
         if (!["light", "dark"].includes(theme)) {
@@ -20,6 +24,7 @@ function ThemeProvider({ children }) {
         if (newTheme === "dark" && !doc.classList.contains("dark")) {
             doc.classList.add("dark");
         }
+        localStorage.setItem("theme", newTheme)
     };
     return (
         <ThemeContext.Provider value={{ theme, changeTheme }}>
