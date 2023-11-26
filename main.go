c4e56fb334d2f349959f572e467b7db3b6005b1a
Áron Noszály <noszalyaron4@gmail.com>
2023-11-26 13:36:00 +0100

    Add profiling option: NJUDGE_PROFILE

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+
 	"github.com/mraron/njudge/cmd"
 )
 
+var profile = os.Getenv("NJUDGE_PROFILE")
+
 func main() {
+	if profile == "true" {
+		fmt.Printf("Creating file %q\n", "cpuprofile")
+		f, err := os.Create("cpuprofile")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		pprof.StartCPUProfile(f)
+
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			for range c {
+				pprof.StopCPUProfile()
+				os.Exit(0)
+			}
+		}()
+	}
+
 	cmd.Execute()
 }
