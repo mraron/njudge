98765eff2a3d8b1c08d18baadec413c82d9ad3d2
Áron Noszály <noszalyaron4@gmail.com>
2024-05-22 13:57:20 +0200

    Remove unnecessary files.

--- internal/web/helpers/helpers.go
+++ internal/web/helpers/helpers.go
@@ -1,21 +1,9 @@
 package helpers
 
 import (
-	"github.com/labstack/echo/v4"
 	"github.com/mraron/njudge/internal/njudge/db/models"
-	"github.com/mraron/njudge/internal/web/templates"
-	"net/http"
 )
 
 func CensorUserPassword(user *models.User) {
 	user.Password = "***CENSORED***"
-}
-
-func LoginRequired(c echo.Context) error {
-	templates.SetFlash(c, "LoginMessage", "A kért oldal megtekintéséhez belépés szükséges!")
-	to := ""
-	if c.Request().Method == "GET" {
-		to = "?next=" + c.Request().URL.Path
-	}
-	return c.Redirect(http.StatusFound, "/user/login"+to)
 }
