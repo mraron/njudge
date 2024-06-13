ac955c463bf7967ee1cdaa8b6660b3343f28dcb4
Áron Noszály <noszalyaron4@gmail.com>
2024-06-13 13:53:09 +0200

    Fix link to profile

--- internal/web/templates/problem_templ.go
+++ internal/web/templates/problem_templ.go
@@ -1436,7 +1436,7 @@ func ProblemRanklist(vm ProblemRanklistViewModel) templ.Component {
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
-				var templ_7745c5c3_Var78 templ.SafeURL = templ.SafeURL("/user/profile/" + row.Name)
+				var templ_7745c5c3_Var78 templ.SafeURL = templ.SafeURL("/user/profile/" + row.Name + "/")
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(string(templ_7745c5c3_Var78)))
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
@@ -1448,7 +1448,7 @@ func ProblemRanklist(vm ProblemRanklistViewModel) templ.Component {
 				var templ_7745c5c3_Var79 string
 				templ_7745c5c3_Var79, templ_7745c5c3_Err = templ.JoinStringErrs(row.Name)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/web/templates/problem.templ`, Line: 316, Col: 80}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/web/templates/problem.templ`, Line: 316, Col: 84}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var79))
 				if templ_7745c5c3_Err != nil {
